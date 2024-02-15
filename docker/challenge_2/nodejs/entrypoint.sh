4eb39d321380571a5bf007babc6bc40961811532
almirpask <almirpask@hotmail.com>
2024-02-14 22:40:12 -0300

    fix entrypoint

--- docker/challenge_2/nodejs/entrypoint.sh
+++ docker/challenge_2/nodejs/entrypoint.sh
@@ -1,18 +1,4 @@
-echo "Waiting for database to start..."
-
-tries=0
-max_tries=30
-
-while ! nc -z $DB_HOST $DB_PORT; do
-    sleep 1
-    tries=$((tries + 1))
-    if [ "$tries" -ge "$max_tries" ]; then
-        echo "Database did not start in time. Exiting."
-        exit 1
-    fi
-done
-
-echo "Database is ready!"
-
+echo "Installing dependencies..."
+npm install
 echo "Starting server"
 node main.js
